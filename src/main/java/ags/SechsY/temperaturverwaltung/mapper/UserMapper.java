4a6c2887ec72c9960d4f5915c0a62edfa5759155
Ebby <tim.eberhardt@gmail.com>
2021-06-26 07:37:46 +0200

    added UserMapper

--- src/main/java/ags/SechsY/temperaturverwaltung/mapper/UserMapper.java
+++ src/main/java/ags/SechsY/temperaturverwaltung/mapper/UserMapper.java
@@ -6,15 +6,23 @@ import ags.SechsY.temperaturverwaltung.model.User;
 public class UserMapper implements BaseMapper<User, UserDTO> {
 
     @Override
-    public UserDTO mapEntity(User e) {
-        // TODO Auto-generated method stub
-        return null;
+    public UserDTO mapEntity(User user) {
+        UserDTO userDto = new UserDTO();
+        userDto.setName(user.getName());
+        userDto.setNickname(user.getNickname());
+        userDto.setPhonenumber(user.getPhonenumber());
+        userDto.setAdmin(user.isAdmin());
+        return userDto;
     }
 
     @Override
-    public User mapDTO(UserDTO d) {
-        // TODO Auto-generated method stub
-        return null;
+    public User mapDTO(UserDTO userDto) {
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setNickname(userDto.getNickname());
+        user.setPhonenumber(userDto.getPhonenumber());
+        user.setAdmin(userDto.isAdmin());
+        return user;
     }
 
 }
