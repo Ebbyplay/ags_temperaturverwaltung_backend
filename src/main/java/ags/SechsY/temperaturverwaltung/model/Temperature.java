8ee1ca5fbcd1b9078755c2f5bbaae4f0944d149b
Ebby <tim.eberhardt@gmail.com>
2021-06-26 14:12:03 +0200

    created temperature entity

---
+++ src/main/java/ags/SechsY/temperaturverwaltung/model/Temperature.java
@@ -0,0 +1,38 @@
+package ags.SechsY.temperaturverwaltung.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "temperature")
+public class Temperature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "temperature_value", nullable = false)
+    private Float temperature_value;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "sensorId", nullable = false)
+    private Sensor sensor;
+}
