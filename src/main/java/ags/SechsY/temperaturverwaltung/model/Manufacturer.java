bf1734eed660c8e4a909e3b3a80f04d58bffa2c4
Ebby <tim.eberhardt@gmail.com>
2021-06-26 14:11:48 +0200

    created manufacturer entity

---
+++ src/main/java/ags/SechsY/temperaturverwaltung/model/Manufacturer.java
@@ -0,0 +1,30 @@
+package ags.SechsY.temperaturverwaltung.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "manufacturer")
+public class Manufacturer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Manufacturer(String name) {
+        this.name = name;
+    }
+}
