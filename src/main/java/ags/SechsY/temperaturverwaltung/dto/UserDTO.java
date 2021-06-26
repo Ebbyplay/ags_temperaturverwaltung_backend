9f16dea193f0a352cf6d537aee3f2be8f6513bb3
Ebby <tim.eberhardt@gmail.com>
2021-06-26 07:30:17 +0200

    added UserDTO

--- src/main/java/ags/SechsY/temperaturverwaltung/dto/UserDTO.java
+++ src/main/java/ags/SechsY/temperaturverwaltung/dto/UserDTO.java
@@ -1,5 +1,13 @@
 package ags.SechsY.temperaturverwaltung.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
+
+    private String name;
+    private String nickname;
+    private String phonenumber;
+    private boolean admin;
 
 }
