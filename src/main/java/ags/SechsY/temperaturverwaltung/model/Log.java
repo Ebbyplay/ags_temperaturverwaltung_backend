59cb989691e08b25c65a6ed1a3de95792710f97d
Ebby <tim.eberhardt@gmail.com>
2021-06-26 08:52:23 +0200

    added manyToOne to log

--- src/main/java/ags/SechsY/temperaturverwaltung/model/Log.java
+++ src/main/java/ags/SechsY/temperaturverwaltung/model/Log.java
@@ -2,11 +2,15 @@ package ags.SechsY.temperaturverwaltung.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -26,4 +30,8 @@ public class Log {
 
     @Column(name = "timestamp", nullable = false)
     private LocalDateTime timestamp = LocalDateTime.now();
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId")
+    private User user;
 }
